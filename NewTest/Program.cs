002bcaabdb07468eef2cb6dc59021bd00cdd641f
DonieMc <mcweeneyd@hotmail.com>
2021-03-03 15:13:23 +0000

    Add files via upload

---
+++ NewTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NewTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string test = "testing";
+           if(test.Length < 10){
+            Console.WriteLine(test.ToString());
+           }
+        }
+        }
+    }
+    
+
